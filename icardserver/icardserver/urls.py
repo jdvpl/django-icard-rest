73e0c1bb3c48dc66aeb36b0f304cd6c8515a9714
jdsuarez18 <jdsuarez@fgs.co>
2025-01-26 18:55:25 -0500

    Add Django REST framework and automatic documentation setup

--- icardserver/icardserver/urls.py
+++ icardserver/icardserver/urls.py
@@ -1,3 +1,4 @@
+
 """icardserver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Icard Docs",
+        default_version='v1',
+        description="Documentacion de la api de Icards",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="juanda554242@gmail.com"),
+        license=openapi.License(name="Juan Daniel Suarez"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
